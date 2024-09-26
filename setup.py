dc3ba6108b69f1b91e10a11f3a4295576abdab44
Mohit Kumar <mkr9395@gmail.com>
2024-09-27 01:49:06 +0530

    requirements.txt installed with changes in setup.py file

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ DESCRIPTION = 'This is the machine learning project with modular coding and skel
 AUTHOR_NAME = 'mohit kumar'
 AUTHOR_EMAIL = 'mohit@python.md'
 
-REQUIREMENTS_FILE_NAME = requirements.txt
+# REQUIREMENTS_FILE_NAME = requirements.txt
 
-def get_requirements_list()-> List(str):
-    with open(REQUIREMENTS_FILE_NAME) as requirements_file:
-        requirement_list = requirements_file.readlines()
-        requirement_list = [for libraries in requirement_list]
+# def get_requirements_list()-> List(str):
+#     with open(REQUIREMENTS_FILE_NAME) as requirements_file:
+#         requirement_list = requirements_file.readlines()
+#         requirement_list = [for libraries in requirement_list]
     
     
     
@@ -29,7 +29,7 @@ setup(
     author_email = AUTHOR_EMAIL,
     url = 'https://github.com/mkr9395',
     # packages = find_packages(where='src'), # find packages inside src folder i.e __init__.py file of src
-    install_requires = get_requirements_list(), # with the help of requirements.txt file we will install this setup file using "-e ."
+    # install_requires = get_requirements_list(), # with the help of requirements.txt file we will install this setup file using "-e ."
     
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")

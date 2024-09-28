5d5b31620424f7660d99d00f5d5d74135093fda7
Mohit Kumar <mkr9395@gmail.com>
2024-09-28 16:28:10 +0530

    custom and box ception with utility functions and logging added

--- demo.py
+++ demo.py
@@ -1,3 +1,13 @@
-from src.logger import logging
+# from src.logger import logging
+from logger import logging
+from src.exceptions import USvisaException
+import sys
 
 logging.info("Welcome to our custom log")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise USvisaException(e, sys)
+
+
